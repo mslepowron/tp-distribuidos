2745ae1cd5da5b08f62e38a7547c0e2e32f87ffb
Fede <fsolariv@fi.uba.ar>
2025-09-26 19:36:45 -0300

    Fix: nueva config del filter

--- workers/filter/filter.py
+++ workers/filter/filter.py
@@ -1,13 +1,16 @@
 import logging
-
-from communication.protocol.deserialize import deserialize_batch
-from communication.protocol.serialize import serialize_row
+import logging
+from uuid import uuid4
+from communication.protocol.deserialize import deserialize_message
+from communication.protocol.serialize import serialize_message
+from communication.protocol.message import Header, HeaderError, PayloadError
+from communication.protocol.schemas import RAW_SCHEMAS
 
 logger = logging.getLogger("filter")
 
 
 class Filter:
-    def __init__(self, mw, result_mw, type='Amount'):
+    def __init__(self, mw, result_mw, type='Year'):
         self.type = type
         self.mw = mw
         self.result_mw = result_mw
@@ -16,7 +19,6 @@ class Filter:
         try:
             logger.info("Waiting for messages...")
             self.start_filter()
-            self.mw.start_consuming(self.callback_filter_amount, queues=["filters_year"]) #Esta cola es especifa de este filter. Pero como hay un exchange no se si hay que nombrar la cola
         except KeyboardInterrupt:
             logger.info("Shutting down consumer...")
             self.mw.stop_consuming()
@@ -35,7 +37,7 @@ class Filter:
 
     def start_filter(self):
         if self.type == 'Amount':
-            self.mw.start_consuming(self.callback_filter_amount, queues=["filters_year"]) #Esta cola es especifa de este filter. Pero como hay un exchange no se si hay que nombrar la cola
+            self.mw.start_consuming(self.callback_filter_amount, queues=["filters_amount"]) #Esta cola es especifa de este filter. Pero como hay un exchange no se si hay que nombrar la cola
         elif self.type == 'Year':
             self.mw.start_consuming(self.callback_filter_year, queues=["filters_year"]) #Esta cola es especifa de este filter. Pero como hay un exchange no se si hay que nombrar la cola
         else:
@@ -43,40 +45,43 @@ class Filter:
 
 
     def callback_filter_amount(self, ch, method, properties, body):
-        filtered_rows = []
-        try:
-            #rows = deserialize_batch(body) comento para probar nuevo protocolo
+        logger.warning(f"IMPLEMENTAR NUEVO PROTOCOLO")
+        # filtered_rows = []
+        # try:
+        #     #rows = deserialize_batch(body) comento para probar nuevo protocolo
 
-            header, rows = deserialize_message(body, RAW_SCHEMAS["transactions.raw"])
-            logger.info(f"Header recibido: {header.as_dictionary()}")
+        #     header, rows = deserialize_message(body, RAW_SCHEMAS["transactions.raw"])
+        #     logger.info(f"Header recibido: {header.as_dictionary()}")
 
-            for row in rows:
-                original_amount = float(row["original_amount"]) if row["original_amount"] else 0.0
-                if original_amount > 75:
-                    filtered_rows.append(row)
+        #     for row in rows:
+        #         original_amount = float(row["original_amount"]) if row["original_amount"] else 0.0
+        #         if original_amount > 75:
+        #             filtered_rows.append(row)
 
-        except Exception as e:
-            logger.error(f"Error procesando mensaje: {e}")
+        # except Exception as e:
+        #     logger.error(f"Error procesando mensaje: {e}")
 
-        logger.info("Imprimo filas")
-        for row in filtered_rows:
-            logger.info(f"F:{row})")
+        # logger.info("Imprimo filas")
+        # for row in filtered_rows:
+        #     logger.info(f"F:{row})")
 
-        ch.basic_ack(delivery_tag=method.delivery_tag)
+        # ch.basic_ack(delivery_tag=method.delivery_tag)
 
-        if filtered_rows:
-            try:
-                csv_bytes = serialize_row(filtered_rows)
-                self.result_mw.send(csv_bytes)
-                logger.info(f"Resultado enviado con {len(filtered_rows)} filas")
-            except Exception as e:
-                logger.error(f"Error enviando resultado: {e}")
+        # if filtered_rows:
+        #     try:
+        #         csv_bytes = serialize_row(filtered_rows)
+        #         self.result_mw.send(csv_bytes)
+        #         logger.info(f"Resultado enviado con {len(filtered_rows)} filas")
+        #     except Exception as e:
+        #         logger.error(f"Error enviando resultado: {e}")
     
     
     def callback_filter_year(self, ch, method, properties, body):
         filtered_rows = []
         try:
-            rows = deserialize_batch(body)
+            header, rows = deserialize_message(body, RAW_SCHEMAS["transactions.raw"])
+            logger.info(f"Header recibido: {header.as_dictionary()}")
+
 
             for row in rows:
                 year = int(row["created_at"].split("-")[0])
@@ -94,8 +99,23 @@ class Filter:
 
         if filtered_rows:
             try:
-                csv_bytes = serialize_row(filtered_rows)  
+                # construir nuevo header
+                result_header = Header({
+                    "message_type": "DATA",
+                    "query_id": header.fields["query_id"],
+                    "stage": "FILTERED",
+                    "part": "transactions.filtered",
+                    "seq": str(uuid4()),
+                    "schema": header.fields["schema"],
+                    "source": "filter"
+                })
+                # serializar correctamente
+                schema_name = header.fields["schema"]
+                schema_fields = RAW_SCHEMAS[schema_name]
+                csv_bytes = serialize_message(result_header, filtered_rows, schema_fields)
+
                 self.result_mw.send(csv_bytes, route_key="coffee_results") #esta es la key que usa el app controller para bindearse con el exchange de results y consumir resultados
+                #result_mw.send(csv_bytes, route_key="year") #esto publicaria directamente con routing a la cola del filter hours
                 logger.info(f"Resultado enviado con {len(filtered_rows)} filas")
             except Exception as e:
                 logger.error(f"Error enviando resultado: {e}")
